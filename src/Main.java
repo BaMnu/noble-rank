327a69a8a569b35b2202affed962baadf615f99d
Olga Fedorova <bamnu13@gmail.com>
2022-08-20 13:01:55 +0300

    Added output for list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Person> noblePeople = new ArrayList<>();
+        noblePeople.add(new Person("Jason", "Brown", 33));
+        noblePeople.add(new Person("Trevor", "Rose Parker Stone", 23));
+        noblePeople.add(new Person("Tilda", "Rose Parker Stone", 60));
+        noblePeople.add(new Person("Marisha", "Walsh Love", 40));
+        noblePeople.add(new Person("Jacob", "Gray Richardson", 26));
+        noblePeople.add(new Person("Peter", "Harris Wilson Walker Owen", 21));
+        noblePeople.add(new Person("Polly", "Harris Wilson Walker Owen", 25));
+
+        int howNoble = 0;
+        System.out.println("Rankings of nobility. From the noblest [1] to the least noble [7].");
+
+        Collections.sort(noblePeople, new PersonSurnameNobilityComparator());
+        for (Person person : noblePeople) {
+            howNoble++;
+            System.out.println(howNoble + ". " + person);
+        }
+
+//        System.out.println(noblePeople);
     }
 }
