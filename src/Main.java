b0426a7e3935630cc45cd3ddb171fe5e7f5151ef
Olga Fedorova <bamnu13@gmail.com>
2022-08-31 13:32:02 +0300

    Deleted comparator class, added lambda in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import jdk.dynalink.Operation;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Person> noblePeople = new TreeSet<>(new PersonSurnameNobilityComparator());
+
+        Comparator<Person> compareNoble = (o1, o2) -> {
+            int maxWords = 3;
+            if ((o1.words >= maxWords) && (o2.words >= maxWords)) {
+                return Integer.compare(o2.getAge(), o1.getAge());
+            } else if (o2.words != o1.words) {
+                return Integer.compare(o2.words, o1.words);
+            } else {
+                return Integer.compare(o2.getAge(), o1.getAge());
+            }
+        };
+
+        Set<Person> noblePeople = new TreeSet<>(compareNoble);
         noblePeople.add(new Person("Jason", "Brown", 33));
         noblePeople.add(new Person("Trevor", "Rose Parker Stone", 23));
         noblePeople.add(new Person("Tilda", "Rose Parker Stone", 60));
@@ -10,6 +24,7 @@ public class Main {
         noblePeople.add(new Person("Jacob", "Gray Richardson", 26));
         noblePeople.add(new Person("Peter", "Harris Wilson Walker Owen", 21));
         noblePeople.add(new Person("Polly", "Harris Wilson Walker Owen", 25));
+
 
         int howNoble = 0;
         System.out.println("Rankings of nobility. From the noblest [1] to the least noble [7].");
